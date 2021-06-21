76e00583d893e091154c28101159c7ce96629607
Andrey Medvedev <gifteddiz@gmail.com>
2021-06-21 18:59:38 +0500

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,59 @@
+$(function(){
+  new Sticky('.js-sticky');
+
+  $(".jsImagePopup").magnificPopup({
+    type: "image",
+  });
+
+  $('.history__item-header').on('click', function(e){
+    e.preventDefault();
+    $(this).siblings('.history__item-cont').slideToggle();
+    $(this).parent().toggleClass('--folded');
+  });
+
+  $.datepicker.regional['ru'] = {
+    closeText: 'Закрыть',
+    prevText: '&#x3C;Пред',
+    nextText: 'След&#x3E;',
+    currentText: 'Сегодня',
+    monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+    monthNamesShort: ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек'],
+    dayNames: ['воскресенье', 'понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота'],
+    dayNamesShort: ['вск', 'пнд', 'втр', 'срд', 'чтв', 'птн', 'сбт'],
+    dayNamesMin: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+    weekHeader: 'Нед',
+    dateFormat: 'dd.mm.yy',
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: '' };
+  $.datepicker.setDefaults($.datepicker.regional['ru']);
+
+  var dateFormat = "mm/dd/yy",
+    from = $( "#dateFrom" )
+      .datepicker({
+        defaultDate: "+1w",
+        numberOfMonths: 1
+      })
+      .on( "change", function() {
+        to.datepicker( "option", "minDate", getDate( this ) );
+      }),
+    to = $( "#dateTo" ).datepicker({
+      defaultDate: "+1w",
+      numberOfMonths: 1
+    })
+    .on( "change", function() {
+      from.datepicker( "option", "maxDate", getDate( this ) );
+    });
+
+  function getDate( element ) {
+    var date;
+    try {
+      date = $.datepicker.parseDate( dateFormat, element.value );
+    } catch( error ) {
+      date = null;
+    }
+
+    return date;
+  }
+})
